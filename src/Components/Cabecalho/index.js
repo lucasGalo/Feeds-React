8e28f2baa55f52132c0a9653e74c94c74d308633
lucas_galo <lucas_d>
2021-01-26 19:08:04 -0300

    Colocando um estilo externo

--- src/Components/Cabecalho/index.js
+++ src/Components/Cabecalho/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Fragment } from 'react';
+import { Text, Image } from 'react-native';
+import estilo from "./estilo.js"
 
-const Cabecalho = ({idUsuario, nomeUsuario}) =>{
-    return(<Text>{idUsuario} - {nomeUsuario}</Text>);
+const Cabecalho = ({ idUsuario, nomeUsuario }) => {
+    return (
+        <Fragment>
+            <Image source={require("../../../res/img/alura.jpg")}
+            style={estilo.fotoUsuario}/>
+            <Text>{idUsuario} - {nomeUsuario}</Text>
+        </Fragment>
+    );
 }
 
 export default Cabecalho;
