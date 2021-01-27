4d19b816d2fe5b1c9b7456fcfa000424280a391e
lucas_galo <lucas_d>
2021-01-27 14:04:20 -0300

    Consumindo api

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import React, { Fragment } from 'react';
-import { ScrollView,  FlatList } from 'react-native';
-import {Cabecalho} from './src/Components/Cabecalho'; /*Trazendo o Cabecalho como um elmento */
-import {Foto} from './src/Components/Foto';
-
-const informacoes = [
-  { id: 1, usuario: "Ricardo" },
-  { id: 2, usuario: "Maria" },
-  { id: 3, usuario: "Jose" }
-]
+import React, { Fragment, useState, useEffect } from 'react';
+import { ScrollView, FlatList } from 'react-native';
+import { Cabecalho } from './src/Components/Cabecalho'; /*Trazendo o Cabecalho como um elmento */
+import { Foto } from './src/Components/Foto';
 
 const App = () => {
+  const [fotos, setFotos] = useState([]);
+  useEffect(() => {
+    const lerFotos = async () => {
+      const fotosHTTP = await fetch("http://192.168.11.2:3030/feed");
+      const fotosJson = await fotosHTTP.json();
+      setFotos(fotosJson);
+    }
+    lerFotos();
+  }, [])
   return (
     <ScrollView>
       <FlatList
-        data={informacoes}
+        data={fotos}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) =>
           <Fragment>
-            <Cabecalho nomeUsuario={item.usuario} />
+            <Cabecalho nomeUsuario={item.userName} />
             <Foto />
           </Fragment>}
       />
