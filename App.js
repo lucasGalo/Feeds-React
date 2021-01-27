23729feee77efe713a687232e09668edfaddcbb4
lucas_galo <lucas_d>
2021-01-27 08:28:23 -0300

    Foto como elemento

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { ScrollView,  FlatList } from 'react-native';
 import {Cabecalho} from './src/Components/Cabecalho'; /*Trazendo o Cabecalho como um elmento */
-import Foto from './src/Components/Foto';
+import {Foto} from './src/Components/Foto';
 
 const informacoes = [
   { id: 1, usuario: "Ricardo" },
--- src/Components/Foto/index.js
+++ src/Components/Foto/index.js
@@ -1,15 +1,1 @@
-import React, { Fragment } from 'react';
-import { Image, Text } from 'react-native';
-import estilo from './estilo';
-
-const Foto = () => {
-  return (
-    <Fragment>
-      <Image style={estilo.imagem}
-        source={require("../../../res/img/alura.jpg")} />
-      <Text>Descrição da foto</Text>
-    </Fragment>
-
-  );
-}
-export default Foto;
+export {default as Foto} from "./Foto"
