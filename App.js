002dbfe4971a0986c656da8b6917e06850f46064
lucas_galo <lucas_d>
2021-01-27 17:37:04 -0300

    Separar, o feed.

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { ScrollView, FlatList } from 'react-native';
+import lerFotos from './src/api/feed';
 import { Cabecalho } from './src/Components/Cabecalho'; /*Trazendo o Cabecalho como um elmento */
 import { Foto } from './src/Components/Foto';
 
 const App = () => {
   const [fotos, setFotos] = useState([]);
   useEffect(() => {
-    const lerFotos = async () => {
-      const fotosHTTP = await fetch("http://192.168.11.2:3030/feed");
-      const fotosJson = await fotosHTTP.json();
-      setFotos(fotosJson);
-    }
-    lerFotos();
+    lerFotos(setFotos);
   }, [])
   return (
     <ScrollView>
