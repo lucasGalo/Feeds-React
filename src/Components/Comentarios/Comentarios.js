bf8166006867e177d8db02a2f728e0d39406ede2
lucas_galo <lucas_d>
2021-01-28 11:53:08 -0300

    pegando e logando dados do input

--- src/Components/Comentarios/Comentarios.js
+++ src/Components/Comentarios/Comentarios.js
@@ -1,22 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Text, FlatList, TextInput, Image, View, TouchableOpacity } from 'react-native';
 import estilo from '../Comentarios/estilo';
 
 const Comentarios = ({ comentarios }) => {
+  const [estComentario, setComentarios] = useState(comentarios)
+  const adicionarComentario = () => {
+    console.warn(conteudoCampoInput);
+  }
+
+  let conteudoCampoInput = "";
+
   return (
     <Fragment>
       <FlatList
-        data={comentarios}
+        data={estComentario}
         keyExtractor={(item, index) => index.id}
         renderItem={({ item }) =>
           <Text>{item.text}</Text>}
       />
       <View style={estilo.naMesmaLinha}>
         <TextInput
+          onChangeText={texto => conteudoCampoInput = texto}
           style={{ flex: 1 }}
           placeholder={"Deixe seu comentário..."}
         />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={adicionarComentario}>
           <Image
             style={estilo.imgSend}
             source={require("../../../res/img/send.png")} />
