d49848be031d6d40abe7ca06cd0e6a16cf2e7d0e
lucas_galo <lucas_d>
2021-02-03 08:08:03 -0300

    Colocando como a tela de feed como principal apos o login

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -16,7 +16,7 @@ const Login = ({ navigation }) => {
       const t = await AsyncStorage.getItem("instalura_token") // obtendo o token, no caso a variável.
 
       //Ir para a tela de Feed
-      navigation.push("Feed", {nome: usuario});
+      navigation.replace("Feed", {nome: usuario});
 
       console.warn(t);
     } catch (erro) {
